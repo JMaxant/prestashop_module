71207fc00bfa3c0efccd1e0ee4702aef4ce98b3b
JMaxant <33941112+JMaxant@users.noreply.github.com>
2018-04-03 15:33:31 +0200

    Create install.sql

---
+++ install/install.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `PREFIXweekspecials`(
+    `id_weekspecials_menu` int(11) NOT NULL AUTO_INCREMENT,
+    `array_weekspecials_menu` text NOT NULL,
+    PRIMARY KEY (`id_weekspecials_menu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
