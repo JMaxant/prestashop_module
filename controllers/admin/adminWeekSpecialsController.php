b5b3ad2d3c68ee161fe23fac1502251128ebc79e
JMaxant <33941112+JMaxant@users.noreply.github.com>
2018-04-11 16:02:25 +0200

    Update adminWeekSpecialsController.php

--- controllers/admin/adminWeekSpecialsController.php
+++ controllers/admin/adminWeekSpecialsController.php
@@ -2,18 +2,38 @@
 
 class AdminWeekSpecialsController extends ModuleAdminController
 {
+    
     public function __construct()
     {
+        $this->name='adminWeekSpecialsController';
         $this->table='weekspecials';
         $this->className='WeekSpecial';
-
         $this->bootstrap=true;
-        
+
         parent::__construct();
     }
 
     public function initContent()
     {
         parent::initContent();
+        // $enable=Configuration::get('WEEKS_ENABLE');
+        // var_dump($enable);
+        $html='test';
+        $this->context->smarty->assign('content',$html);
     }
+
+    
+    // public function initContent()
+    // {
+    //     parent::initContent();
+    //     $content=$this->context->smarty->fetch(_PS_MODULE_DIR.'weekspecials/views/template/admin/content.tpl');
+    //     var_dump($content);
+    //     die;
+        
+    //     // $this->context->smarty->assign(array(
+    //     //     'content'=>$this->content.$content,
+    //     //     )
+    //     // );
+    // }
+
 }
