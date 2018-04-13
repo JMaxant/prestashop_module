e6fbf834150dafc37ddd61e1e8b030f5547e9dce
JMaxant <33941112+JMaxant@users.noreply.github.com>
2018-04-13 13:44:57 +0200

    Added courses_weekspecials_menu table

--- install/install.sql
+++ install/install.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS `PREFIXweekspecials`(
     `id_weekspecials_menu` int(11) NOT NULL AUTO_INCREMENT,
+    `courses_weekspecials_menu` text NOT NULL,
     `array_weekspecials_menu` text NOT NULL,
     PRIMARY KEY (`id_weekspecials_menu`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
