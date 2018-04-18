4369388e443ef1194efea5f6c293886f31c8b10d
JMaxant <julien.maxant.pro@gmail.com>
2018-04-18 12:03:13 +0200

    Updated to match modified db

--- controllers/hook/displayHomeTab.php
+++ controllers/hook/displayHomeTab.php
@@ -13,9 +13,11 @@ class WeekSpecialsDisplayHomeTabController
         // affichage hook displayHomeTab
     public function run()
     {   
-        $row='array_weekspecials_menu';
+        $row='*';
         $req=WeekSpecial::getRows($row);
-        $output=unserialize($req[$row]);
+        $courses=unserialize($req['courses_weekspecials_menu']);
+        $output=unserialize($req['array_weekspecials_menu']);
+
         $args=array_keys($output);
         foreach($args as $arg){
             if($arg=='date'){
@@ -30,6 +32,7 @@ class WeekSpecialsDisplayHomeTabController
         }
         $this->context->smarty->assign('dates',$formatDates);
         $this->context->smarty->assign('menu', $menu);
+        $this->context->smarty->assign('courses',$courses);
         $ws_days=array('Lundi','Mardi','Mercredi','Jeudi','Vendredi');
         $this->context->smarty->assign('ws_days',$ws_days);
         return $this->module->display($this->file, 'displayHomeTab.tpl');
